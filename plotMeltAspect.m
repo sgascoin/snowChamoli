ca5ce654850806910e4d7d5a09be0a6d72c3f41f
Simon Gascoin <simon.gascoin@cesbio.cnes.fr>
2021-03-04 21:18:22 +0100

    "add code for melt aspect"

---
+++ plotMeltAspect.m
@@ -0,0 +1,19 @@
+%% Check aspect of melt areas
+za=geotiffread('ROIbb2/aspect');
+% Sentinel-2 FSC product from MAJA-LIS (Gascoin et al. 2019)
+d1='05feb'; 
+d2='10feb';
+f1=geotiffread(['ROIbb2/fsc' d1]);
+f2=geotiffread(['ROIbb2/fsc' d2]);
+m = f1>0 & f2==0 & f1<=100 & f2<=100 ; 
+c = f1<=100 & f2<=100 ; 
+%%
+figure(1),clf
+[t,rc]=rose(deg2rad(za(c)),deg2rad(0:18:360));
+[~,rm]=rose(deg2rad(za(m)),deg2rad(0:18:360));
+r=rm./rc;
+r(isnan(r))=0;
+polar(t,r,'-')
+% north up and angles increasing anti-clockwise 
+view(90, -90);
+title('Polar histogram: areas of snowmelt between 05 and 10 Feb')
