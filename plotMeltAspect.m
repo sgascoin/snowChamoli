a1908122bb60df316a5970d9d9e99a921f2f4e1e
Simon Gascoin <simon.gascoin@cesbio.cnes.fr>
2021-03-04 22:05:35 +0100

    compute aspect in code

--- plotMeltAspect.m
+++ plotMeltAspect.m
@@ -1,13 +1,18 @@
 %% Check aspect of melt areas
-za=geotiffread('ROIbb2/aspect');
-% Sentinel-2 FSC product from MAJA-LIS (Gascoin et al. 2019)
+% compute aspect in deg from the DEM
+z=geotiffread('ROIbb2/dem');
+[fx,fy] = gradient(z);
+za=rad2deg(wrapToPi(atan2(-fx,fy)));
+% load Sentinel-2 FSC product from MAJA-LIS (Gascoin et al. 2019)
 d1='05feb'; 
 d2='10feb';
 f1=geotiffread(['ROIbb2/fsc' d1]);
 f2=geotiffread(['ROIbb2/fsc' d2]);
+% Mask of melt areas
 m = f1>0 & f2==0 & f1<=100 & f2<=100 ; 
+% Mask of clear-sky areas
 c = f1<=100 & f2<=100 ; 
-%%
+%% Plot polar histogram
 figure(1),clf
 [t,rc]=rose(deg2rad(za(c)),deg2rad(0:18:360));
 [~,rm]=rose(deg2rad(za(m)),deg2rad(0:18:360));
@@ -17,4 +22,4 @@ polar(t,r,'-')
 % north up and angles increasing anti-clockwise 
 view(90, -90);
 title({'Snowmelt area between 05 and 10 Feb', '% of clear-sky pixels by aspect bin'})
-saveas(1,'html/meltAspect.png')
+%saveas(1,'html/meltAspect.png')
